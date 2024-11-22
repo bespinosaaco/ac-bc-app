54a1b8d9d99fee55c457c059d3c96f4ecca31119
Brian Espinosa-Acosta <brianespinosaacosta@gmail.com>
2024-11-22 15:38:25 -0330

    reading a google drive folder

--- ac-bc_app.py
+++ ac-bc_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import hmac
 import pandas as pd
+import requests
+import os
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -28,19 +30,47 @@ def check_password():
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
-# Main Streamlit app starts here
-st.write("Here goes your normal Streamlit app...")
-st.button("Click me")
+# The google import solution
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
 
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
+
+folder = "G:\My Drive\AC-BC"
+os.chdir(folder)
+files = os.listdir()
+
+# Main Streamlit app starts here
 st.title("AC/BC 🦦")
 st.header("Atlantic Canada Biochar Project")
 st.write("Made by Brian Espinosa Acosta")
-df = pd.DataFrame(
-    [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
-    ]
-)
 
-st.dataframe(df, use_container_width=True)
+st.write("Files in directory:")
+st.write(files)
+
+# df = pd.read_csv(path)
+
+# st.dataframe(df, use_container_width=True)
